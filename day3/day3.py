9e962853532317ed8f957e893b3bd84caa3fd84e
Nacer Dergal <nacer.dergal@hiloenergie.com>
2021-12-05 01:08:19 -0500

    finished day3

--- day3/day3.py
+++ day3/day3.py
@@ -7,3 +7,21 @@ gamma = "".join(
     for bits in zip(*data)
 )
 print(int(gamma[::2], 2) * int(gamma[1::2], 2))
+
+def func(data,cmp):
+    for i in range(len(data[0])):
+        _01 = { "0" : [], "1" : []}
+        for line in data:
+            _01[line[i]].append(line)
+        if len(data := _01[
+                "1" if cmp(len(_01["1"]), len(_01["0"])) else "0"
+            ]) == 1:
+                return int(data[0], 2)
+
+    print(data)
+
+print(func(data[:],int.__ge__) * func(data[:],int.__lt__))
+
+
+
+    
